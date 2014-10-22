9f59c2718609aca3455e8276846b6c4ad17ffebb
Stefan Arentz <stefan@arentz.ca>
2014-10-22 10:46:42 -0400

    Added a basic SyncStorageClient

---
+++ sync/client.go
@@ -0,0 +1,202 @@
+package sync
+
+import (
+	"bytes"
+	"code.google.com/p/go.crypto/hkdf"
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/base64"
+	"encoding/hex"
+	"encoding/json"
+	"errors"
+	"github.com/st3fan/gofxa/fxa"
+	"io"
+	"io/ioutil"
+	"net/http"
+)
+
+type EncryptedPayload struct {
+	CipherText string `json:"ciphertext"`
+	IV         string `json:"IV"`
+	HMAC       string `json:"HMAC"`
+}
+
+type Record struct {
+	Id       string  `json:"id"`
+	Modified float64 `json:"modified"`
+	Payload  string  `json:"payload"`
+}
+
+func (r *Record) Decrypt(keyBundle KeyBundle) error {
+	encryptedPayload := &EncryptedPayload{}
+	if err := json.Unmarshal([]byte(r.Payload), encryptedPayload); err != nil {
+		return errors.New("Record does not have an encrypted payload")
+	}
+
+	HMAC, err := hex.DecodeString(encryptedPayload.HMAC)
+	if err != nil {
+		return errors.New("Malformed HMAC in record")
+	}
+
+	// Verify the payload
+
+	mac := hmac.New(sha256.New, keyBundle.ValidationKey)
+	mac.Write([]byte(encryptedPayload.CipherText))
+	if !bytes.Equal(mac.Sum(nil), HMAC) {
+		return errors.New("Record validation failed")
+	}
+
+	// Decrypt the payload
+
+	ciphertext, err := base64.StdEncoding.DecodeString(encryptedPayload.CipherText)
+	if err != nil {
+		return errors.New("Malformed ciphertext in record")
+	}
+
+	iv, err := base64.StdEncoding.DecodeString(encryptedPayload.IV)
+	if err != nil {
+		return errors.New("Malformed IV in record")
+	}
+
+	block, err := aes.NewCipher(keyBundle.EncryptionKey)
+	if err != nil {
+		return err
+	}
+
+	mode := cipher.NewCBCDecrypter(block, iv)
+	mode.CryptBlocks(ciphertext, ciphertext)
+
+	// TODO: Is there a stdlib something to deal with PKCS7 padding?
+	length := len(ciphertext)
+	unpadding := int(ciphertext[length-1])
+	ciphertext = ciphertext[:(length - unpadding)]
+
+	r.Payload = string(ciphertext)
+
+	return nil
+}
+
+//
+
+type KeyBundle struct {
+	EncryptionKey []byte
+	ValidationKey []byte
+}
+
+func NewKeyBundle(key []byte) (KeyBundle, error) {
+	secret := make([]byte, 2*32)
+	if _, err := io.ReadFull(hkdf.New(sha256.New, key, nil, []byte("identity.mozilla.com/picl/v1/oldsync")), secret); err != nil {
+		return KeyBundle{}, err
+	}
+	return KeyBundle{
+		EncryptionKey: secret[0:32],
+		ValidationKey: secret[32:64],
+	}, nil
+}
+
+//
+
+type StorageClient struct {
+	endpoint  string
+	hawkKeyId string
+	hawkKey   string
+	secret    []byte
+}
+
+func NewStorageClient(endpoint, hawkKeyId, hawkKey string, secret []byte) (*StorageClient, error) {
+	return &StorageClient{
+		endpoint:  endpoint,
+		hawkKeyId: hawkKeyId,
+		hawkKey:   hawkKey,
+		secret:    secret,
+	}, nil
+}
+
+type KeysPayload struct {
+	Default []string `json:"default"`
+}
+
+func (sc *StorageClient) FetchKeys() (KeyBundle, error) {
+	globalKeyBundle, err := NewKeyBundle(sc.secret)
+	if err != nil {
+		return KeyBundle{}, err
+	}
+
+	record, err := sc.GetEncryptedRecord("crypto", "keys", globalKeyBundle)
+	if err != nil {
+		return KeyBundle{}, err
+	}
+
+	keysPayload := KeysPayload{}
+	if err = json.Unmarshal([]byte(record.Payload), &keysPayload); err != nil {
+		return KeyBundle{}, err
+	}
+
+	encryptionKey, err := base64.StdEncoding.DecodeString(keysPayload.Default[0])
+	if err != nil {
+		return KeyBundle{}, err
+	}
+
+	validationKey, err := base64.StdEncoding.DecodeString(keysPayload.Default[1])
+	if err != nil {
+		return KeyBundle{}, err
+	}
+
+	return KeyBundle{
+		EncryptionKey: encryptionKey,
+		ValidationKey: validationKey,
+	}, nil
+}
+
+func (sc *StorageClient) GetRecord(collectionName, recordId string) (Record, error) {
+	req, err := http.NewRequest("GET", sc.endpoint+"/storage/"+collectionName+"/"+recordId, nil)
+	if err != nil {
+		return Record{}, err
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Accept", "application/json")
+
+	hawkCredentials := fxa.NewHawkCredentials(sc.hawkKeyId, []byte(sc.hawkKey))
+	if err := hawkCredentials.AuthorizeRequest(req, nil, ""); err != nil {
+		return Record{}, err
+	}
+
+	client := &http.Client{}
+	res, err := client.Do(req)
+	if err != nil {
+		return Record{}, err
+	}
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return Record{}, err
+	}
+
+	if res.StatusCode != http.StatusOK {
+		return Record{}, errors.New(res.Status) // TODO: Proper errors based on what the server returns
+	}
+
+	record := Record{}
+	if err = json.Unmarshal(body, &record); err != nil {
+		return Record{}, err
+	}
+
+	return record, nil
+}
+
+func (sc *StorageClient) GetEncryptedRecord(collectionName, recordId string, keyBundle KeyBundle) (Record, error) {
+	record, err := sc.GetRecord(collectionName, recordId)
+	if err != nil {
+		return Record{}, err
+	}
+
+	if err := record.Decrypt(keyBundle); err != nil {
+		return Record{}, err
+	}
+
+	return record, nil
+}
--- syncapi/router.go
+++ syncapi/router.go
@@ -5,11 +5,25 @@
 package syncapi
 
 import (
+	"crypto/dsa"
+	"crypto/rand"
+	"crypto/sha256"
 	"encoding/base64"
+	"encoding/hex"
 	"github.com/gorilla/mux"
-	//"github.com/st3fan/gofxa/fxa"
+	"github.com/st3fan/gofxa/fxa"
+	"github.com/st3fan/gowebtoken/webtoken"
+	"github.com/st3fan/moz-syncapi/sync"
+	"github.com/st3fan/moz-syncapi/tokenclient"
+	"log"
 	"net/http"
 	"strings"
+	"time"
+)
+
+const (
+	ASSERTION_OFFSET   = time.Duration(15) * time.Second
+	ASSERTION_DURATION = time.Duration(24*60*60) * time.Second
 )
 
 type Application struct {
@@ -17,11 +31,37 @@ type Application struct {
 }
 
 type Credentials struct {
-	Username string
-	Password string
+	Username    string
+	Password    string
+	KeyA        []byte
+	KeyB        []byte
+	ApiEndpoint string
+	ApiKeyId    string
+	ApiKey      string
+}
+
+func generateRandomKey() (*dsa.PrivateKey, error) {
+	params := new(dsa.Parameters)
+	if err := dsa.GenerateParameters(params, rand.Reader, dsa.L1024N160); err != nil {
+		return nil, err
+	}
+	priv := new(dsa.PrivateKey)
+	priv.PublicKey.Parameters = *params
+	if err := dsa.GenerateKey(priv, rand.Reader); err != nil {
+		return nil, err
+	}
+	return priv, nil
+}
+
+func deriveClientStateFromKey(key []byte) string {
+	hash := sha256.New()
+	hash.Write(key)
+	return hex.EncodeToString(hash.Sum(nil)[0:16])
 }
 
 func (app *Application) authenticate(w http.ResponseWriter, r *http.Request) *Credentials {
+	// Grab the credentials from basic auth
+
 	authorization := r.Header.Get("Authorization")
 	if len(authorization) == 0 {
 		http.Error(w, "Unauthorized", http.StatusUnauthorized)
@@ -50,25 +90,94 @@ func (app *Application) authenticate(w http.ResponseWriter, r *http.Request) *Cr
 		return nil
 	}
 
-	//client, err := fxa.Client()
+	// Do the FxA dance
+
+	client, err := fxa.NewClient(usernameAndPassword[0], usernameAndPassword[1])
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return nil
+	}
+
+	if err := client.Login(); err != nil {
+		http.Error(w, "Authorization failed: "+err.Error(), http.StatusUnauthorized)
+		return nil
+	}
+
+	if err := client.FetchKeys(); err != nil {
+		http.Error(w, "Authorization failed: "+err.Error(), http.StatusUnauthorized)
+		return nil
+	}
+
+	key, err := generateRandomKey()
+	if err != nil {
+		log.Fatal("Could not generate DSA key: ", err)
+	}
+
+	cert, err := client.SignCertificate(key)
+	if err != nil {
+		http.Error(w, "Authorization failed: "+err.Error(), http.StatusUnauthorized)
+		return nil
+	}
+
+	// Turn the certificate into an assertion
+
+	issuedAt := time.Now().Add(-ASSERTION_OFFSET)
+	expiresAt := issuedAt.Add(ASSERTION_DURATION)
+
+	assertion, err := webtoken.CreateAssertion(*key, cert, "https://token.services.mozilla.com", "127.0.0.1", issuedAt, expiresAt)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return nil
+	}
+
+	// Send the certificate to the token server
+
+	tokenClient, _ := tokenclient.New()
+	tokenServerResponse, err := tokenClient.ExchangeToken(assertion, "sync", "1.5", deriveClientStateFromKey(client.KeyB))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return nil
+	}
+
+	//
 
 	return &Credentials{
-		Username: usernameAndPassword[0],
-		Password: usernameAndPassword[1],
+		Username:    usernameAndPassword[0],
+		Password:    usernameAndPassword[1],
+		KeyA:        client.KeyA,
+		KeyB:        client.KeyB,
+		ApiEndpoint: tokenServerResponse.ApiEndpoint,
+		ApiKeyId:    tokenServerResponse.Id,
+		ApiKey:      tokenServerResponse.Key,
 	}
 }
 
+//
+
 func (app *Application) HandleProfile(w http.ResponseWriter, r *http.Request) {
 	if credentials := app.authenticate(w, r); credentials != nil {
-
 	}
 }
 
 func (app *Application) HandleTabs(w http.ResponseWriter, r *http.Request) {
 	if credentials := app.authenticate(w, r); credentials != nil {
+		storageClient, err := sync.NewStorageClient(credentials.ApiEndpoint, credentials.ApiKeyId, credentials.ApiKey, credentials.KeyB)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
 
+		keyBundle, err := storageClient.FetchKeys()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		log.Print("KEY BUNDLE: ", keyBundle)
 	}
 }
+
+//
 
 func SetupRouter(r *mux.Router, config Config) (*Application, error) {
 	app := &Application{config: config}
